b0f566e6c9549f41e7d12eefa29d65ebcf6c379c
OlivierLem <lemort.olivier.pro@gmail.com>
2023-03-24 16:22:46 +0100

    integration footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,32 @@ import styles from './Footer.module.scss'
 export default function Footer () {
     return (
         <div className={`${styles.footer}`}>
-            Footer
+            <div className={`${styles.footer_content}`}>
+                <p>Logo</p>
+                <ul>
+                    <li className={`${styles.active}`}>Thémes</li>
+                    <li><a href="#">Histoire</a> </li>
+                    <li><a href="#"> Harry potter</a></li>
+                    <li><a href="#"> One piece </a></li>
+                    <li><a href="#"> developpement web </a></li>
+                </ul>
+                <ul>
+                    <li className={`${styles.active}`}>Catégorie</li>
+                    <li><a href="#">Sport</a></li>
+                    <li><a href="#">Film et séries</a></li>
+                    <li><a href="#">Etudes</a></li>
+                    <li><a href="#">jeux vidéo</a></li>
+                </ul>
+                <ul>
+                    <li><a href="#">Mentions légales</a></li>
+                    <li><a href="#">Politique de confidentialités</a></li>
+                    <li><a href="#">Aides et informations</a></li>
+                    <li><a href="#">Contact</a></li>   
+                </ul>
+            </div>
+            <div className={`${styles.footer_bottom}`}>
+                <p>© 2023 One Quizz. All Rights Reserved.</p>
+            </div>
         </div>
     )
 }
