cda395dc16a07872ea80576b149b79660baa0e63
OlivierLem <lemort.olivier.pro@gmail.com>
2023-03-29 16:15:03 +0200

    ajout formulaire inscription dans le composant connexion

--- src/components/connexion/Connexion.jsx
+++ src/components/connexion/Connexion.jsx
@@ -1,12 +1,25 @@
+import { useState } from 'react'
 import styles from './Connexion.module.scss'
 
 export default function Connexion () {
+
+    const[reveal, setReveal] = useState(false)
+
+    function handleClick () {
+        setReveal(true)
+    }
+
+    function handleRemove (e) {
+        e.stopPropagation()
+        setReveal(false)
+    }
+
     return (
-        <div className={`${styles.connexion}`}>
+        <div className={`${styles.connexion}  ${reveal && styles.active}`}>
             <form action="">
-                <h2>Connexion au compte</h2>
+                <h3>Connexion au compte</h3>
                 <div>
-                    <label htmlFor="pseudo">Pseudo</label>
+                   <label htmlFor="pseudo">Pseudo</label>
                     <input type="text" name="pseudo"  />
                 </div>
                 <div>
@@ -17,14 +30,35 @@ export default function Connexion () {
                     <input type="checkbox" name="remember" />
                     <label htmlFor="remember">Se souvenir de moi</label>
                 </span>
-                <button>Se connecter</button>
+                <button>Se connecter</button> 
             </form>
-            <div>
+            <div onClick={handleClick} >
                 <div>
                     <span></span>
                     <span></span>
                 </div>
-                
+                <form action="">
+                    <h3>Inscription</h3>
+                    <i className="fa-solid fa-xmark"
+                       onClick={handleRemove}></i>
+                    <div>
+                        <label htmlFor="email">Email</label>
+                        <input type="text" name="email"  />
+                    </div>
+                    <div>
+                        <label htmlFor="pseudo">Pseudo</label>
+                        <input type="text" name="pseudo"  />
+                    </div>
+                    <div>
+                        <label htmlFor="password">Mots de passe</label>
+                        <input type="password" name="password"  />
+                    </div>
+                    <div>
+                        <label htmlFor="confirm_password">confirmation mots de passe</label>
+                        <input type="password" name="confirm_password"  />
+                    </div>
+                    <button>S'inscrire</button> 
+                </form>
             </div>
         </div>
     )
